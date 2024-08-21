629c0208dc42bd12b597b6e628578bda942f1e38
Weeraluk <weeraluksop.mayawizard@gmail.com>
2024-08-22 03:07:36 +0700

    add : dayjs & form & reset form &  set form

---
+++ src/pages/Form/type.ts
@@ -0,0 +1,32 @@
+export interface DataUser {
+  id: string;
+  title: string;
+  firstName: string;
+  lastName: string;
+  birthday: string;
+  nationality: string;
+  cityzenId: string;
+  gender: string;
+  mobileNumber: string;
+  passport: string;
+  expectedSalary: string;
+}
+
+export interface FormType {
+  id: string;
+  title: string;
+  firstName: string;
+  lastName: string;
+  birthday: string;
+  nationality: string;
+  citizenId: string;
+  citizenIdPart2: string;
+  citizenIdPart3: string;
+  citizenIdPart4: string;
+  citizenIdPart5: string;
+  gender: string;
+  mobile: string;
+  mobile2: string;
+  passport: string;
+  expectedSalary: string;
+}
